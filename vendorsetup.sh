29d8247e32ea5e2d545628b8418d8307b49ddd78
smile349 <122448836+smile349@users.noreply.github.com>
2024-06-15 12:03:03 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,24 +1,5 @@
 #
 #	This file is part of the OrangeFox Recovery Project
-# 	Copyright (C) 2019-2021 The OrangeFox Recovery Project
-#	
-#	OrangeFox is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	any later version.
-#
-#	OrangeFox is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-# 	This software is released under GPL version 3 or any later version.
-#	See <http://www.gnu.org/licenses/>.
-# 	
-# 	Please maintain this if you use this script or any part of it
-#
-#
-#	This file is part of the OrangeFox Recovery Project
 # 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
 #
 #	OrangeFox is free software: you can redistribute it and/or modify
@@ -54,11 +35,11 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-   	export TW_DEFAULT_LANGUAGE="en"
+   	export TW_DEFAULT_LANGUAGE="zh_CH"
 	export LC_ALL="C"
  	export ALLOW_MISSING_DEPENDENCIES=true
-	export TARGET_DEVICE_ALT="haydn,haydnin"
-	export OF_TARGET_DEVICES="haydn,haydnin"
+	export TARGET_DEVICE_ALT="star,starin"
+	export OF_TARGET_DEVICES="star,starin"
 	export OF_AB_DEVICE=1
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
@@ -92,13 +73,13 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
     # R11.1
     export FOX_VERSION="R11.1_0_000"
-    export OF_MAINTAINER="Sushrut1101"
+    export OF_MAINTAINER="Nachoneko"
     export OF_MAINTAINER_AVATAR="misc/Sushrut1101.png"
     export FOX_BUILD_TYPE="Developer-Preview"
     
 	# Screen Settings
-	export OF_SCREEN_H=2400
-	export OF_STATUS_H=110
+	export OF_SCREEN_H=2340
+	export OF_STATUS_H=80
 	export OF_STATUS_INDENT_LEFT=48
 	export OF_STATUS_INDENT_RIGHT=48
 	export OF_CLOCK_POS=1
